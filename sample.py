aa738e4090c951a103d8ed280abad70f37b020e8
Subham Mandal <subhammandal01@gmail.com>
2019-08-08 19:41:17 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -5,3 +5,13 @@ user_name = input('Please enter the name of the user: ')
 print('Adding user {}'.format(user_name))
 subprocess.run(['useradd','-m','-d','/home/'+user_name,'-s','/bin/bash',user_name])
 root@rcxdev:~#
+
+  #!/usr/bin/python
+
+import subprocess
+user_name = input('Please enter the name of the user: ')
+if user_name in open('/etc/passwd').read():
+     print('User {} already present'.format(user_name))
+else:
+     print('Adding user {}'.format(user_name))
+     subprocess.run(['useradd','-m','-d','/home/'+user_name,'-s','/bin/bash',user_name])
