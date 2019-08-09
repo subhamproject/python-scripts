a2fb4112153a73c6bd26fcc83cffedb776e52d54
Subham Mandal <subhammandal01@gmail.com>
2019-08-09 14:00:02 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,3 +1,22 @@
+
+#!/usr/bin/python
+
+import subprocess
+import os
+
+with open(os.devnull, 'w') as DEVNULL:
+  user = input('Please enter the name of the user to add: ')
+  for user_name in user.split(','):
+      status = subprocess.run(['grep',user_name,'/etc/passwd'],stdout=DEVNULL, stderr=DEVNULL)
+      if status.returncode == 0:
+         print('User \"{}\" already present'.format(user_name))
+      else:
+         print('Adding user \"{}\"'.format(user_name))
+         status_code = subprocess.run(['useradd','-m','-d','/home/'+user_name,'-s','/bin/bash',user_name])
+         if status_code.returncode == 0:
+            print('User \"{}\" was successfully added'.format(user_name))
+
+
 #!/usr/bin/python3
 
 import subprocess, re
