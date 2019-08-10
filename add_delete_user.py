82786403b51abbb5d66c7910bdac3903a6cc0d74
Subham Mandal <subhammandal01@gmail.com>
2019-08-10 20:12:18 +0530

    Update add_delete_user.py

--- add_delete_user.py
+++ add_delete_user.py
@@ -1,3 +1,57 @@
+
+
+#!/usr/bin/python
+
+import subprocess
+import os
+import argparse
+import pwd
+import sys
+import getpass
+import re
+
+add_user = argparse.ArgumentParser(description='Add/Delete Users From Server')
+add_user.add_argument('-a','--add',metavar='UserName',nargs='+',required=False,help='Add user to server',dest='username')
+add_user.add_argument('-d','--del',metavar='UserName',nargs='+',required=False,help='Add user to server',dest='deluser')
+args = add_user.parse_args()
+user = args.username
+del_user = args.deluser
+
+def root_check():
+    ''' Exit if login name not root.'''
+
+    if not getpass.getuser() == 'root':
+        raise SystemExit('ERROR: THIS PROGRAM REQUIRES ROOT PRIVILEGES. EXITING.')
+
+def add_user(usersname):
+    for user_name in usersname:
+         try:
+            pwd.getpwnam(user_name)
+            print('User \"{}\" already present'.format(user_name))
+         except KeyError:
+            print('Adding user \"{}\"'.format(user_name))
+            status_code = subprocess.run(['useradd','-m','-d','/home/'+user_name,'-s','/bin/bash',user_name])
+            if status_code.returncode == 0:
+               print('User \"{}\" was successfully added'.format(user_name))
+
+def delete_user(usersname):
+    for d_user in usersname:
+        try:
+           pwd.getpwnam(d_user)
+           print('Deleting user \"{}\"'.format(d_user))
+           status_code = subprocess.run(['userdel','-r',d_user])
+           if status_code.returncode == 0:
+              print('User \"{}\" was successfully deleted'.format(d_user))
+        except KeyError:
+           print('User \"{}\" not present'.format(d_user))
+
+if __name__ == '__main__':
+    if user:
+       add_user(user)
+    elif del_user:
+       delete_user(del_user)
+
+    ############################################################################################################################
 ## First way
 #!/usr/bin/python
 
